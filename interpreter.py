f9bcc59e917f54e53de252a4fe4e428e642abf0e
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2020-08-24 20:41:05 -0400

    Added math operations

--- interpreter.py
+++ interpreter.py
@@ -3,25 +3,31 @@ PATH = "main.ez"
 lines = []
 
 with open(PATH, "r") as f:
-    for index, value in enumerate(f.readlines()):
-        lines.append(value.strip("\n"))
-    lines = [i for i in lines if i] 
+  for index, value in enumerate(f.readlines()):
+    lines.append(value.strip("\n"))
+  lines = [i for i in lines if i] 
 
 
 def operate_num(line: list) -> float:
-    num_list = []
-    for num in line[1:]:
-        num_list.append(float(num))
-    return sum(num_list)
+  if line[2] == "+":
+    return float(line[1]) + float(line[3])
+  if line[2] == "-":
+    return float(line[1]) - float(line[3])
+  if line[2] == "*":
+    return float(line[1]) * float(line[3])
+  if line[2] == "/":
+    return float(line[1]) / float(line[3])
+  if line[2] == "^":
+    return float(line[1]) ** float(line[3])
 
 
 def scan_lines(lines: list):
-    for line in lines:
-        split_line = line.split()
-        if "output" in line:
-            print(" ".join(split_line[1:]))
-        if "add" in line:
-            print(operate_num(split_line))
+  for line in lines:
+    split_line = line.split()
+    if "output" in line:
+      print(" ".join(split_line[1:]))
+    if "operate" in line:
+      print(operate_num(split_line))
 
 
 scan_lines(lines)
