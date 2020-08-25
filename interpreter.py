9c0654a242fd67575dd420f3ade37e1781b1910a
John Lins <46113320+JohnLins@users.noreply.github.com>
2020-08-24 17:24:58 -0700

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -15,6 +15,46 @@ def operate_num(line: list) -> float:
         num_list.append(float(num))
     return sum(num_list)
 
+def check_if(line: list) -> float:
+  
+    args = []
+    for arg in line[1:]:
+        args.append(arg)
+    if args[1] == "==":
+      if args[0] == args[2]:
+        return "true"
+      else:
+        return "false"
+        
+    elif args[1] == "<":
+      if args[0] < args[2]:
+        return "true"
+      else:
+        return "false"
+
+    elif args[1] == ">":
+      if args[0] > args[2]:
+        return "true"
+      else:
+        return "false"
+
+    elif args[1] == "<=":
+      if args[0] <= args[2]:
+        return "true"
+      else:
+        return "false"
+
+    elif args[1] == "!=":
+      if args[0] != args[2]:
+        return "true"
+      else:
+        return "false"
+
+    elif args[1] == ">=":
+      if args[0] >= args[2]:
+        return "true"
+      else:
+        return "false"
 
 # Scans the line for key words
 def scan_lines(lines: list):
@@ -24,6 +64,8 @@ def scan_lines(lines: list):
             print(" ".join(split_line[1:]))
         if "add" in line:
             print(operate_num(split_line))
+        if "if" in line:
+            print(check_if(split_line))
 
 
 scan_lines(lines)
